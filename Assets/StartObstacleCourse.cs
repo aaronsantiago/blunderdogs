bb91978eaf83ee3276d600ed39913e273576628d
Aaron Santiago <me@aaron.work>
2023-07-15 13:10:24 -0400

    reorganize files and add intro that disappears when touched

--- Assets/StartObstacleCourse.cs
+++ Assets/StartObstacleCourse.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class StartObstacleCourse : MonoBehaviour
 {
     public GameObject Environment;
+    public GameObject Intro;
     private void OnTriggerEnter(Collider other)
     {
         Environment.SetActive(true);
+        Intro.SetActive(false);
     }
 }
