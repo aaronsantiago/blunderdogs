81f389a37617bdc5ec2f0875bffbb6ce829a82f9
Aaron Santiago <me@aaron.work>
2023-07-15 17:52:17 -0400

    tweaks to get lvl1 to run properly

--- Assets/StartObstacleCourse.cs
+++ Assets/StartObstacleCourse.cs
@@ -6,9 +6,17 @@ public class StartObstacleCourse : MonoBehaviour
 {
     public GameObject Environment;
     public GameObject Intro;
+
+    public static string RecordingName = "ObstacleCourse";
+
+    public List<Transform> TransformsToRecord;
     private void OnTriggerEnter(Collider other)
     {
         Environment.SetActive(true);
         Intro.SetActive(false);
+
+        RecordingName = System.Guid.NewGuid().ToString();
+
+        RecordingManager.isRecording[RecordingName] = TransformsToRecord;
     }
 }
