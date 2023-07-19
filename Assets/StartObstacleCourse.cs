d525491199fa481af3435c77ea077424fcd24fb0
unknown <aaronsantiago+github@gmail.com>
2023-07-19 05:41:31 -0400

    Update start of levels to require all colliders and use stand here rather than touch

--- Assets/StartObstacleCourse.cs
+++ Assets/StartObstacleCourse.cs
@@ -7,7 +7,23 @@ public class StartObstacleCourse : MonoBehaviour
     public GameObject Environment;
     public GameObject Intro;
 
+    private List<Collider> _colliders = new List<Collider>();
+
     private void OnTriggerEnter(Collider other)
+    {
+        _colliders.Add(other);
+        if (_colliders.Count == 3)
+        {
+            startObstacleCourse();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _colliders.Remove(other);
+    }
+
+    void startObstacleCourse()
     {
         GhostManager.Instance.SpawnAllReplays();
         GhostManager.Instance.StartRecording();
